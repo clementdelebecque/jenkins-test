88ad7916e10436b60a4b66b733cbe26585b5a892
Clément Delebecque <clement.delebecque@outlook.fr>
2018-10-09 23:17:39 +0200

    commit 1

---
+++ src/main/java/fr/esipe/App.java
@@ -0,0 +1,13 @@
+package fr.esipe;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
